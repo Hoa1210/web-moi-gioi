be62ccebbba1c71a45acd4d4dddf79bd20fff310
LuongHoa <luonghoa121002@gmail.com>
2022-07-13 19:50:16 +0700

    login register by github and gitlab

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,72 +18,86 @@
 </head>
 
 <body class="loading authentication-bg" data-layout-config='{"darkMode":false}'>
-<div class="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-xxl-4 col-lg-5">
-                <div class="card">
+<div class="auth-fluid">
+    <!--Auth fluid left content -->
+    <div class="auth-fluid-form-box">
+        <div class="align-items-center d-flex h-100">
+            <div class="card-body">
 
-                    <!-- Logo -->
-                    <div class="card-header pt-4 pb-4 text-center bg-primary">
-                        <a href="index.html">
-                            <span><img src="{{asset('images/logo.png')}}" alt="" height="18"></span>
-                        </a>
+                <!-- Logo -->
+                <div class="auth-brand text-center text-lg-start">
+                    <a href="index.html" class="logo-dark">
+                        <span><img src="{{asset('images/logo-dark.png')}}" alt="" height="18"></span>
+                    </a>
+                    <a href="index.html" class="logo-light">
+                        <span><img src="{{asset('images/logo.png')}}" alt="" height="18"></span>
+                    </a>
+                </div>
+
+                <!-- title-->
+                <h4 class="mt-0">Sign In</h4>
+                <p class="text-muted mb-4">Enter your email address and password to access account.</p>
+
+                <!-- form -->
+                <form action="#">
+                    <div class="mb-3">
+                        <label for="emailaddress" class="form-label">Email address</label>
+                        <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email">
                     </div>
-
-                    <div class="card-body p-4">
-
-                        <div class="text-center w-75 m-auto">
-                            <h4 class="text-dark-50 text-center pb-0 fw-bold">Sign In</h4>
-                            <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
+                    <div class="mb-3">
+                        <a href="pages-recoverpw-2.html" class="text-muted float-end"><small>Forgot your password?</small></a>
+                        <label for="password" class="form-label">Password</label>
+                        <input class="form-control" type="password" required="" id="password" placeholder="Enter your password">
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="checkbox-signin">
+                            <label class="form-check-label" for="checkbox-signin">Remember me</label>
                         </div>
+                    </div>
+                    <div class="d-grid mb-0 text-center">
+                        <button class="btn btn-primary" type="submit"><i class="mdi mdi-login"></i> Log In </button>
+                    </div>
+                    <!-- social-->
+                    <div class="text-center mt-4">
+                        <p class="text-muted font-16">Sign in with</p>
+                        <ul class="social-list list-inline mt-3">
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-linkedin"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="{{route('auth.redirect','gitlab')}}" class="social-list-item border-warning text-warning"><i class="mdi mdi-gitlab"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="{{route('auth.redirect','github')}}" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </form>
+                <!-- end form-->
 
-                        <form action="#">
+                <!-- Footer-->
+                <footer class="footer footer-alt">
+                    <p class="text-muted">Don't have an account? <a href="pages-register-2.html" class="text-muted ms-1"><b>Sign Up</b></a></p>
+                </footer>
 
-                            <div class="mb-3">
-                                <label for="emailaddress" class="form-label">Email address</label>
-                                <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email">
-                            </div>
-
-                            <div class="mb-3">
-                                <a href="pages-recoverpw.html" class="text-muted float-end"><small>Forgot your password?</small></a>
-                                <label for="password" class="form-label">Password</label>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" placeholder="Enter your password">
-                                    <div class="input-group-text" data-password="false">
-                                        <span class="password-eye"></span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-3">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
-                                    <label class="form-check-label" for="checkbox-signin">Remember me</label>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-0 text-center">
-                                <button class="btn btn-primary" type="submit"> Log In </button>
-                            </div>
-
-                        </form>
-                    </div> <!-- end card-body -->
-                </div>
-                <!-- end card -->
-
-                <div class="row mt-3">
-                    <div class="col-12 text-center">
-                        <p class="text-muted">Don't have an account? <a href="pages-register.html" class="text-muted ms-1"><b>Sign Up</b></a></p>
-                    </div> <!-- end col -->
-                </div>
-                <!-- end row -->
-
-            </div> <!-- end col -->
-        </div>
-        <!-- end row -->
+            </div> <!-- end .card-body -->
+        </div> <!-- end .align-items-center.d-flex.h-100-->
     </div>
-    <!-- end container -->
+    <!-- end auth-fluid-form-box-->
+
+    <!-- Auth fluid right content -->
+    <div class="auth-fluid-right text-center">
+        <div class="auth-user-testimonial">
+            <h2 class="mb-3">I love the color!</h2>
+            <p class="lead"><i class="mdi mdi-format-quote-open"></i> It's a elegent templete. I love it very much! . <i class="mdi mdi-format-quote-close"></i>
+            </p>
+            <p>
+                - Hyper Admin User
+            </p>
+        </div> <!-- end auth-user-testimonial-->
+    </div>
+    <!-- end Auth fluid right content -->
 </div>
 <!-- end page -->
 
